6904c61b0259a5ce3fca90b1868f1c669034c35b
Oscar <racsodruade0@gmail.com>
2022-03-16 00:13:29 -0600

    Creando formulario

--- ig_app.py
+++ ig_app.py
@@ -33,24 +33,14 @@ def analizar():
         messagebox.showerror(message="Error, no se a cargado archivo para analizar", title="Alerta")    
     
     #listas de almacenamiento
-    global cadena, tokn, error
+    global formslist, tokn, error
     tokn = []
     error = []
     lista_valores = {}
     formslist = []
-    tempvalores = {}
     columna = 1
     fila = 1
     temp = ""
-    tem = ""
-    #tokns
-    #palabra reservada
-    reservada = ""
-    variable = ""
-    asignacion = ""
-    contenido = ""
-    separador = ""
-    #variables
     formulario = ""
     conF = ""
     tipo = ""
@@ -58,7 +48,6 @@ def analizar():
     fondo = ""
     nombre = ""
     valores = ""
-    form_vacio = True
     formval = True
     conFval = True
     tipoval = False
@@ -69,7 +58,6 @@ def analizar():
     valista = False
     nform = False
     form = False
-    escomilla = False
     #Inicio del autómata
     for letra in data:
              
@@ -355,7 +343,7 @@ def analizar():
                     elif letra == "]":
                         tokentemp = "Token cierre de documento ' "+letra+" ' encontrado en Lin. "+str(fila)+", col. "+str(columna)
                         tokn.append(tokentemp)    
-
+    generar_form()
 
     
         
@@ -471,7 +459,6 @@ def rep_error():
     except Exception as e:
         messagebox.showerror(message="Error, no se a cargado o analizado ningúna información", title="Alerta")
     
-    print(error)
     if error == "":
 
         f = open('Reporte_error.html', 'w')  
@@ -569,6 +556,150 @@ def rep_error():
         file = webbrowser.open('Reporte_error.html')  
     else:
         messagebox.showerror(message="Felicidades no tienes errores", title="Alerta")
+
+def generar_form():
+    try:
+        "analizar()"
+    except Exception as e:
+        messagebox.showerror(message="Error, no se a cargado o analizado ningúna información", title="Alerta")
+    f = open('Formulario.html', 'w')  
+    etiquetaval = False
+    textoval = False
+    radioval = False
+    optionval = False
+    botonval = False
+
+    for a in range(len(formslist)):
+        tipo = formslist[a]['tipo']       
+
+
+        if tipo == "etiqueta":
+            etiquetaval = True
+            html_etiqueta = """
+            <h3>
+            {}
+            </h3>            
+            """.format(formslist[a]['valor'])
+        elif tipo == "texto":
+            print(formslist[a]['fondo'])
+            textoval = True
+            html_texto = """
+            <input type="text" name={} size="40" placeholder={}>
+            """.format(formslist[a]['valor'],"\""+formslist[a]['fondo']+"\"")
+        elif tipo == "grupo-radio":
+            radioval = True
+            vlores = formslist[a]['valores'].replace("'","").split(",")
+            print(vlores)
+
+            html_r1 = """
+            <form>
+            <h3>{}</h3>
+            """.format(formslist[a]['nombre'])
+
+            for i in range(len(vlores)):                
+                r = vlores[i]
+                print(r)
+                html_r2 = """            
+                <input type="radio" name="radio" value={}>
+                <label for={}>{}</label>            
+            """.format(r,"contactChoice"+str(i),r)
+
+            html_r3 = """
+            </div>
+            </form>
+            """
+            html_radio = html_r1 + html_r2 + html_r2
+        elif tipo == "grupo-option":
+            vlores = formslist[a]['valores'].replace("'","").split(",")
+            print(vlores)
+        elif tipo == "boton":
+            vlores = formslist[a]['valores']
+            print(vlores)
+
+    html_cabeza = """
+        <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Reporte</title>
+        </head>
+
+        <body>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand"> &nbsp;&nbsp;&nbsp;Reporte</a>
+        </nav>
+
+        """
+    html_pie="""   
+        <br><br><br><br><br><br>
+        <footer>
+        </footer>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+        </body>
+        <style>
+        table {
+        border: #b2b2b2 1px solid;
+        border-collapse: separate;
+        
+        }
+        th {
+        border: black 1px solid;
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #357baa;
+        color: white;
+        }
+        td, th {
+        border: 1px solid #ddd;
+        padding: 8px;
+        }
+        
+        tr:nth-child(even){background-color: #c0c0c0;}
+        
+        tr:hover {background-color: #ddd;}
+        
+        
+        </style>
+
+        </body>
+        """
+    
+    if etiquetaval is True:
+        html_etiqueta = html_etiqueta
+    else:
+        html_etiqueta = ""
+    
+    if textoval is True:
+        html_texto = html_texto
+    else:
+        html_texto = ""
+
+    if radioval is True:
+        html_radio = html_radio
+    else:
+        html_radio = ""
+
+    html = html_cabeza  + html_etiqueta + html_texto + html_radio + html_pie        
+    f.write(html)     
+    f.close()     
+    file = webbrowser.open('Formulario.html')  
+    
     
 
     # -----------------------------------------------------------------------------------------------------------------------
